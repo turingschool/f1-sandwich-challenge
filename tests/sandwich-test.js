bd12167e39a7bb5da875d8e7527aff4ab893d3f6
Khalid Williams <khalidwill27@gmail.com>
2019-04-08 16:40:53 -0600

    Add skips into tests

--- tests/sandwich-test.js
+++ tests/sandwich-test.js
@@ -4,19 +4,19 @@ var Sandwich = require('../exercises/sandwich');
 describe('Sandwich', function() {
 
 
-  it('Should instantiate an object', function() {
+  it.skip('Should instantiate an object', function() {
     var sandwich = new Sandwich('Club');
 
     assert.isObject(sandwich)
   })
 
-  it('Should have a type', function() {
+  it.skip('Should have a type', function() {
     var sandwich = new Sandwich('PB&J')
 
     assert.equal(sandwich.type, 'PB&J')
   })
 
-  it('Should have no ingredients by default', function() {
+  it.skip('Should have no ingredients by default', function() {
     var sandwich = new Sandwich('Club');
 
     assert.equal(sandwich.bread, null);
@@ -24,7 +24,7 @@ describe('Sandwich', function() {
     assert.deepEqual(sandwich.fillings, []);
   })
 
-  it('Should be able to recieve ingredients', function() {
+  it.skip('Should be able to recieve ingredients', function() {
     var ingredients = ['ham', 'cheese'];
 
     var hamAndCheese = new Sandwich('Ham and Cheese', 'sourdough', ingredients);
@@ -34,7 +34,7 @@ describe('Sandwich', function() {
     assert.equal(hamAndCheese.bread, 'sourdough');
   })
 
-  it('Should not be toasted by default', function() {
+  it.skip('Should not be toasted by default', function() {
     var bbq = new Sandwich('BBQ','white bun', ['pulled pork', 'BBQ sauce', 'coleslaw', 'Texas Pete']);
 
     assert.equal(bbq['toasted'], false);
--- tests/shop-test.js
+++ tests/shop-test.js
@@ -3,13 +3,13 @@ var Shop = require('../exercises/shop');
 var Sandwich = require('../exercises/sandwich')
 
 describe('Shop', function() {
-  it('should not have a menu by default', function() {
+  it.skip('should not have a menu by default', function() {
     var sandwichJoint = new Shop();
   
     assert.deepEqual(sandwichJoint.menu, [])
   });
 
-  it('should be able to toast a sandwich', function() {
+  it.skip('should be able to toast a sandwich', function() {
     var sandwichShop = new Shop();
     var meatballSub = new Sandwich('Meatball Sub', 'hoagie roll', ['meatballs', 'parmesean cheese', 'oregano']);
 
@@ -17,7 +17,7 @@ describe('Shop', function() {
     assert.equal(toastedSandwich.toasted, true);
   })
 
-  it('should be able to make a menu', function() {
+  it.skip('should be able to make a menu', function() {
     var sandwichJoint = new Shop();
     var cucumberSandwich = new Sandwich('Cucumber Sandwich','white', ['cucumber', 'cream cheeser', 'mayo', 'dill'])
     var meatballSub = new Sandwich('Meatball Sub', 'hoagie roll', ['meatballs', 'parmesean cheese', 'oregano'])
@@ -28,7 +28,7 @@ describe('Shop', function() {
     assert.deepEqual(sandwichJoint.menu, [cucumberSandwich, meatballSub, turkeySammy])
   })
 
-  it('Should have 5 of each sandwich to start', function() {
+  it.skip('Should have 5 of each sandwich to start', function() {
     var sandwichJoint = new Shop();
     var cucumberSandwich = new Sandwich('Cucumber Sandwich','white', ['cucumber', 'cream cheeser', 'mayo', 'dill'])
     var meatballSub = new Sandwich('Meatball Sub', 'hoagie roll', ['meatballs', 'parmesean cheese', 'oregano'])
@@ -43,7 +43,7 @@ describe('Shop', function() {
     assert.deepEqual(sandwichJoint.stock[2], {sandwich:'Turkey Sandwich', quantity: 5})
   })
 
-  it('Should be able to take orders', function() {
+  it.skip('Should be able to take orders', function() {
     var sandwichJoint = new Shop();
 
     var pbj = new Sandwich( 'Peanut Butter & Jelly', 'white', ['peanut butter', 'jelly'])
